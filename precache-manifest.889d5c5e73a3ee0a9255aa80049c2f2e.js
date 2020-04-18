76c1c059fb774612677362f679972e5f440b03ee
haider-ali-1 <haider.ali.developer123@gmail.com>
2020-04-18 17:17:52 +0500

    Updates

---
+++ precache-manifest.889d5c5e73a3ee0a9255aa80049c2f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1c2b96b2f38079cb0b3c502b818f34",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7015e13f55de8fc0cc02",
+    "url": "/monsters-rolodex/static/css/main.1e6a3dea.chunk.css"
+  },
+  {
+    "revision": "b6916b2889f34763c035",
+    "url": "/monsters-rolodex/static/js/2.3dc452b4.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/monsters-rolodex/static/js/2.3dc452b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7015e13f55de8fc0cc02",
+    "url": "/monsters-rolodex/static/js/main.bfa37fe3.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
